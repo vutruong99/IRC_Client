fb0c27a66b94d2bb9f6f5d616682e7c0181d8b74
tda1312 <tda1312@users.noreply.github.com>
2019-11-27 18:11:24 +0700

    Top nav bar + Navigation Drawer

--- IRCClient/app/src/main/java/com/example/ircclient/MessageActivity.java
+++ IRCClient/app/src/main/java/com/example/ircclient/MessageActivity.java
@@ -1,17 +1,31 @@
 package com.example.ircclient;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.SearchView;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+import android.widget.ToggleButton;
 
 import org.w3c.dom.Text;
-
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -20,20 +34,19 @@ import java.util.Dictionary;
 import java.util.List;
 import java.util.Calendar;
 
-
 public class MessageActivity extends AppCompatActivity {
+    private RelativeLayout layout;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
 
-
-        TextView greetUser = (TextView) findViewById(R.id.greetUser); //Great user
-        Intent myIntent = getIntent();
-        String nick = myIntent.getStringExtra("nick");
-
-        greetUser.setText("Hello " + nick + " !");
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        configureNavigationDrawer();
+        configureToolbar();
 
         ListView listView = (ListView) findViewById(R.id.message_container); //Get list view
 
@@ -75,5 +88,62 @@ public class MessageActivity extends AppCompatActivity {
         //Set the view to the adapter
         listView.setAdapter(adapter);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.drawer_menu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void configureToolbar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_more);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void configureNavigationDrawer(){
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+
+        /*NavigationView navView = (NavigationView) findViewById(R.id.navigation);
+        navView.setNavigationItemSelectedListener((new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            //work with items in navigation view
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment f = null;
+                int itemId = menuItem.getItemId();
+
+                // on Item click
+                if(f != null){
+                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id., f);
+                    transaction.commit();
+                    drawerLayout.closeDrawer();
+                    return true;
+                }
+
+                return false;
+            }
+        }));*/
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int itemId = item.getItemId();
+
+        switch (itemId){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return true;
     }
 }
